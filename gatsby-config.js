52a868895889ff734ba9770af1ffed7d92d1d8ab
chaharshivam <chahar.shivam@yahoo.com>
2019-07-08 23:43:40 +0530

    Update social links

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,7 @@ module.exports = {
     social: {
       github: 'chaharshivam',
       linkedin: 'shivamchahar',
-      twitter: '/shahr_96',
+      twitter: 'shahr_96',
     },
   },
   plugins: [
